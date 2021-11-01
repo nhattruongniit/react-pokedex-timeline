af5007c9adfee60047738c8b909c654cc04254a1
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-11-01 22:14:03 +0700

    response

--- src/App.js
+++ src/App.js
@@ -5,44 +5,47 @@ function App() {
       <h1>Timeline</h1>
 
       <div className="timeline_wrapper">
-        <div className="timeline_block timeline_even">
-          <div className="timeline_monster">
-            <img src="assets/images/monster-pikachu.png" alt="" />
+        <div className="timeline_container">
+          <div className="timeline_block timeline_even">
+            <div className="timeline_monster">
+              <img src="assets/images/monster-pikachu.png" alt="" />
+            </div>
+            <img className="block-common block-2" src="assets/images/block-orange.svg" alt="" />
+            <img className="block-common block-3" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-4" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-5" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-6" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-7" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-8" src="assets/images/block-blue.svg" alt="" />
           </div>
-          <img className="block-common block-2" src="assets/images/block-orange.svg" alt="" />
-          <img className="block-common block-3" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-4" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-5" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-6" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-7" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-8" src="assets/images/block-blue.svg" alt="" />
-        </div>
 
-        <div className="timeline_block timeline_odd">
-          <div className="timeline_monster">
-            <img src="assets/images/monster-charmember.png" alt="" />
+          <div className="timeline_block timeline_odd">
+            <div className="timeline_monster">
+              <img src="assets/images/monster-charmember.png" alt="" />
+            </div>
+            <img className="block-common block-2" src="assets/images/block-orange.svg" alt="" />
+            <img className="block-common block-3" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-4" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-5" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-6" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-7" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-8" src="assets/images/block-blue.svg" alt="" />
           </div>
-          <img className="block-common block-2" src="assets/images/block-orange.svg" alt="" />
-          <img className="block-common block-3" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-4" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-5" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-6" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-7" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-8" src="assets/images/block-blue.svg" alt="" />
-        </div>
 
-        <div className="timeline_block timeline_even">
-          <div className="timeline_monster">
-            <img src="assets/images/monster-charmember.png" alt="" />
+          <div className="timeline_block timeline_even">
+            <div className="timeline_monster">
+              <img src="assets/images/monster-charmember.png" alt="" />
+            </div>
+            <img className="block-common block-2" src="assets/images/block-orange.svg" alt="" />
+            <img className="block-common block-3" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-4" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-5" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-6" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-7" src="assets/images/block-blue.svg" alt="" />
+            <img className="block-common block-8" src="assets/images/block-blue.svg" alt="" />
           </div>
-          <img className="block-common block-2" src="assets/images/block-orange.svg" alt="" />
-          <img className="block-common block-3" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-4" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-5" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-6" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-7" src="assets/images/block-blue.svg" alt="" />
-          <img className="block-common block-8" src="assets/images/block-blue.svg" alt="" />
         </div>
+        
       </div>
     </main>
   );
